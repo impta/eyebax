d5680edfa057ea3b78a31ec22457e30e4da8b6b5
impta <87300042+impta@users.noreply.github.com>
2022-02-02 04:29:30 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ else
   echo "Cloning $BRANCH branch...."
   git clone https://github.com/subinps/ahubbax -b $BRANCH /ahubbaxx
 fi
-cd /VCPlayerBot
+cd /ahubbaxx
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
