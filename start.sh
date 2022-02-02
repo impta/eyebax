d4ab8e7be4eef7de52000b33314964b329dbf220
impta <87300042+impta@users.noreply.github.com>
2022-02-02 04:57:48 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/impta/abubbax /ahubbax
+  git clone https://github.com/subinps/VCPlayerBot /VCPlayerBot
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/impta/ahubbax -b $BRANCH /ahubbax
+  git clone https://github.com/subinps/VCPlayerBot -b $BRANCH /VCPlayerBot
 fi
-cd /ahubbax
+cd /VCPlayerBot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
