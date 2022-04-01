48c892b252fbba9b6c8f7b4f11a8261fc6d98ca4
impta <87300042+impta@users.noreply.github.com>
2022-04-01 08:28:57 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@
 from pyrogram import Client
 from config import Config
 bot = Client(
-    "ahubbax",
+    "VCPlayerBot",
     Config.API_ID,
     Config.API_HASH,
     bot_token=Config.BOT_TOKEN,
